7a9e06fd3d4037c37b655e14c8adfe7d1e1b0f2c
225038 <zhurbovaa@mail.ru>
2018-01-22 11:19:43 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  data: any;
+  val2: number;
+  val3: number;
+  val1: number;
+  constructor() {
+    this.data = {
+      labels: ['-R', '-R/2', '0', 'R/2', 'R'],
+      datasets: [
+        {
+          label: 'График',
+          data: [0, 2.5, 2.5, 4, 5],
+          fill: false,
+          borderColor: '#4bc0c0'
+        }
+      ]
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {MatSliderModule} from '@angular/material/slider';
+import {ChartModule, InputTextModule, ButtonModule} from 'primeng/primeng';
 import { AppComponent } from './app.component';
-
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatSliderModule, ChartModule, InputTextModule, ButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
